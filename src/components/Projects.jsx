e10bbb344b996eb0ac32711c5b5426fe249c3442
pknowledge <rumjogg.gmail.com>
2026-09-07 21:59:37 -0400

    fixed broken code

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -11,7 +11,7 @@ const projects = [
     },
   { id: '02', 
     title: 'Infinity Checker', 
-    description: 'An interactive, simple web app designed for checking closeness between compatibility between lovers/friends/family members meant for fun',
+    description: 'An interactive, simple web app designed for checking closeness between lovers/friends/family members meant for fun',
     stack: 'HTML | CSS | Vanilla Java Script',
     pending: false, 
     complete: true,
